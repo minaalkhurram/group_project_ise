8450dbcda6a6b7e08a793c4b847f55dbaf2cc7fb
minaalkhurram <102298342+minaalkhurram@users.noreply.github.com>
2022-05-11 00:17:44 +0500

    Update Source.cpp

--- lesco_project/Source.cpp
+++ lesco_project/Source.cpp
@@ -1,8 +1,72 @@
 #include<iostream>
+#include<fstream>
+#include<string.h>
+#include<string>
+
 using namespace std;
 
 int main()
 {
-	cout << "test";
-cout<<"test 2";
+
+	char check;
+	cout << " Are you an employee or a Customer? \n\n Enter e for employee and c for customer ";
+	cin >> check;
+
+
+	if (check == 'e')
+	{
+		string username;
+		string password;
+		cout << "ENTER USERNAME :";
+		cin >> username;
+		cout << "ENTER PASSWORD : ";
+		cin >> password;
+
+		ifstream fin;
+		string line;
+		fin.open("employeesdata.txt");
+		if (fin.is_open())
+		{
+			bool check2=true;
+			while (getline(fin, line))
+			{
+				
+				for (int i = 0; username[i] != '\0'; i++)
+				{
+					if (username[i] == line[i])
+					{
+						check2 = true;
+					}
+					else
+						check2=false;
+
+				}
+				if (check2)
+				{
+					
+					int size = username.length();
+					for (int i = size+1, j = 0; password[j] != '\0'; i++, j++)
+					{
+						if (password[j] == line[i])
+						{
+							check2 = true;
+
+						}
+						else
+							check2 = false;
+					}
+
+				}
+				if (check2)
+				{
+					cout << "LOGIN SUCCESFUL";
+
+				}
+
+
+			}
+
+		}
+
+	}
 }
