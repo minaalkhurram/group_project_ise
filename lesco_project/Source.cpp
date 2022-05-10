cd608c7eedf2e7303c399f8ebb849f43df6f2b73
minaalkhurram <102298342+minaalkhurram@users.noreply.github.com>
2022-05-11 00:46:50 +0500

    Update Source.cpp

--- lesco_project/Source.cpp
+++ lesco_project/Source.cpp
@@ -5,6 +5,114 @@
 
 using namespace std;
 
+bool employee(string username, string password)
+{
+		ifstream fin;
+	string line;
+	bool check2 = false;
+	fin.open("employeesdata.txt");
+	if (fin.is_open())
+	{
+
+		while (getline(fin, line))
+		{
+
+			for (int i = 0; username[i] != '\0'; i++)
+			{
+				if (username[i] == line[i]&&line[i]!=',')
+				{
+					check2 = true;
+				}
+				else
+					check2 = false;
+
+			}
+			if (check2)
+			{
+
+				int size = username.length();
+				for (int i = size + 1, j = 0; password[j] != '\0'; i++, j++)
+				{
+					if (password[j] == line[i]&&line[i]!='\0')
+					{
+						check2 = true;
+
+					}
+					else
+						check2 = false;
+				}
+
+			}
+			if (check2)
+			{
+				cout << "LOGIN SUCCESFUL";
+				break;
+			}
+			else
+			{
+				cout << "USERNAME OR PASS INCORRECT ";
+				break;
+
+			}
+		}
+		}
+
+	return check2;
+
+
+		
+}
+
+
+bool customer(string username, string password)
+{
+	ifstream fin;
+	string line;
+	bool check2 = false;
+	fin.open("customersinfo.txt");
+	if (fin.is_open())
+	{
+
+		while (getline(fin, line))
+		{
+
+			for (int i = 0; username[i] != '\0'; i++)
+			{
+				if (username[i] == line[i]&&line[i]!=',')
+				{
+					check2 = true;
+				}
+				else
+					check2 = false;
+
+			}
+			if (check2)
+			{
+
+				int size = username.length();
+				for (int i = size + 1, j = 0; password[j] != '\0'; i++, j++)
+				{
+					if (password[j] == line[i]&&line[i]!='\0')
+					{
+						check2 = true;
+
+					}
+					else
+						check2 = false;
+				}
+
+			}
+			if (check2)
+			{
+				cout << "LOGIN SUCCESFUL";
+				break;
+			}
+		}
+	}
+
+	return check2;
+}
+
 int main()
 {
 
@@ -12,61 +120,37 @@ int main()
 	cout << " Are you an employee or a Customer? \n\n Enter e for employee and c for customer ";
 	cin >> check;
 
-
-	if (check == 'e')
+	string username;
+	string password;
+	cout << "ENTER USERNAME :";
+	cin >> username;
+	cout << "ENTER PASSWORD : ";
+	cin >> password;
+	bool login = false;
+	int m = 0;
+	while (m == 0 || m == 1)
 	{
-		string username;
-		string password;
-		cout << "ENTER USERNAME :";
-		cin >> username;
-		cout << "ENTER PASSWORD : ";
-		cin >> password;
 
-		ifstream fin;
-		string line;
-		fin.open("employeesdata.txt");
-		if (fin.is_open())
+		if (check == 'e')
 		{
-			bool check2=true;
-			while (getline(fin, line))
-			{
-				
-				for (int i = 0; username[i] != '\0'; i++)
-				{
-					if (username[i] == line[i])
-					{
-						check2 = true;
-					}
-					else
-						check2=false;
-
-				}
-				if (check2)
-				{
-					
-					int size = username.length();
-					for (int i = size+1, j = 0; password[j] != '\0'; i++, j++)
-					{
-						if (password[j] == line[i])
-						{
-							check2 = true;
-
-						}
-						else
-							check2 = false;
-					}
-
-				}
-				if (check2)
-				{
-					cout << "LOGIN SUCCESFUL";
-
-				}
-
-
-			}
+			login = employee(username, password);
 
 		}
+		else
+		{
+			login = customer(username, password);
+		}
+		if (login == false)
+		{
+			cout << " PRESS 1 TO RETRY AND 2 TO EXIT ";
+			cin >> m;
+		}
 
+		if (m == 2)
+		{
+			exit(0);
+
+
+		}
 	}
-}
+	}
